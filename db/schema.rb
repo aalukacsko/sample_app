1aa045ebed0b1ca01b6be2d3e99b5e61afa991cd
Aaron L <publicaaron@theallthing.com>
2010-03-05 23:29:25 -0500

    Finished first cut of the User Model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100215042026) do
+ActiveRecord::Schema.define(:version => 20100306035704) do
 
   create_table "users", :force => true do |t|
     t.string   "name",       :null => false
@@ -17,5 +17,7 @@ ActiveRecord::Schema.define(:version => 20100215042026) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
